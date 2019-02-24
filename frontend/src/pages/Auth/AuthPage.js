f8ba4aaeebd724a1c6eef89557a5f65b18b5f57f
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-02-24 19:17:10 +0300

    add simple form submithandler

--- frontend/src/pages/Auth/AuthPage.js
+++ frontend/src/pages/Auth/AuthPage.js
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import './AuthPage.css';
 
 const AuthPage = (props) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const emailEL = useRef(null);
+  const passwordEL = useRef(null);
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const email = emailEL.current.value;
+    const password = passwordEL.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) return;
+
+    console.log(email, password);
+  };
 
   return (
-    <form className="auth-form">
+    <form className="auth-form" onSubmit={submitHandler}>
       <div className="form-control">
         <label htmlFor="email">Email</label>
-        <input type="text" id="email" />
+        <input type="text" id="email" ref={emailEL} />
       </div>
       <div className="form-control">
         <label htmlFor="password">Password</label>
-        <input type="password" id="email" />
+        <input type="password" id="email" ref={passwordEL} />
       </div>
       <div className="form-actions">
         <button type="button">Sign UP</button>
