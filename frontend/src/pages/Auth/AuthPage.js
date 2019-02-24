0282abafa7565b9b991c8f56f5282a1bd5189e8c
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-02-24 19:29:02 +0300

    add CORS backend

--- frontend/src/pages/Auth/AuthPage.js
+++ frontend/src/pages/Auth/AuthPage.js
@@ -13,6 +13,24 @@ const AuthPage = (props) => {
     if (email.trim().length === 0 || password.trim().length === 0) return;
 
     console.log(email, password);
+    const requestBody = {
+      query: `
+        mutation {
+          createUser(userInput: {email: "${email}", password: "${password}"}) {
+            _id
+            email
+          }
+        }
+      `
+    };
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
   };
 
   return (
