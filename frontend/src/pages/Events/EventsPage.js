2ac3124df048f66722280f43d9511ff3ab3ae794
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-02-25 15:41:02 +0300

    add modal form

--- frontend/src/pages/Events/EventsPage.js
+++ frontend/src/pages/Events/EventsPage.js
@@ -1,14 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './EventsPage.css';
 import Modal from '../../components/Modal/Modal';
 import Backdrop from '../../components/Backdrop/Backdrop';
 
 const EventsPage = (props) => {
   const [creating, setCreating] = useState(false);
+  const titleElRef = useRef(null);
+  const priceElRef = useRef(null);
+  const dateElRef = useRef(null);
+  const descriptionElRef = useRef(null);
 
   const startCreateEventHandler = () => setCreating(true);
-  const modalConfirmHandler = () => setCreating(false);
+  const modalConfirmHandler = () => {
+
+    const title = titleElRef.current.value;
+    const price = priceElRef.current.value;
+    const date = dateElRef.current.value;
+    const description = descriptionElRef.current.value;
+
+    if (
+      title.trim().length === 0 ||
+      price <= 0 ||
+      date.trim().length === 0 ||
+      description.trim().length === 0
+    ) {
+      return;
+    }
+
+    const event = { title, price, date, description };
+    console.log('event from confirm =>', event);
+
+    setCreating(false);
+  };
   const modalCancelHandler = () => setCreating(false);
+
+  useEffect(() => {
+    console.log('events page ');
+  }, []);
 
   return (
     <React.Fragment>
@@ -21,7 +49,24 @@ const EventsPage = (props) => {
           canCancel
           canConfirm
         >
-          <p>Modal content</p>
+          <form>
+            <div className="form-control">
+              <label htmlFor="title">Title</label>
+              <input type="text" id="title" ref={titleElRef} />
+            </div>
+            <div className="form-control">
+              <label htmlFor="price">Price</label>
+              <input type="number" id="price" ref={priceElRef} />
+            </div>
+            <div className="form-control">
+              <label htmlFor="date">Date</label>
+              <input type="datetime-local" id="date" ref={dateElRef} />
+            </div>
+            <div className="form-control">
+              <label htmlFor="description">Description</label>
+              <textarea id="description" rows="4" ref={descriptionElRef} />
+            </div>
+          </form>
         </Modal>
       )}
       <h2>Events page</h2>
